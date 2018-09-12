8d9bfc89b47f99c94543557c0a0b1f9e897b49ca
Apostolos Zezos <apostolos.zezos@jrosefield-9649.local>
2018-09-12 14:02:22 -0400

    TakingInput

--- TakingInputInt.py
+++ TakingInputInt.py
@@ -1,9 +1,21 @@
 #Input
 #Assignment Statement
+
 r = input("What is the radius: ")
+r = int(r)
+#Casting is the process of changing type
+#Strings - collections of characters
+#int - integers
+#float - numbers with decimals
+
 h = input("What is the height: ")
+h = int(h)
 
 #Process
 
+sa = (2 * (3.14) * r * h) + (2 * (3.14) * (r * r))
+
 #Output
+
+print(sa)
 
